510d14488c2069266631b0515ebd7484871674ab
tsikhonya <tsikhonya98@gmail.com>
2022-01-17 20:51:26 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApp1
+{
+    public class PowerList
+    {
+        public IEnumerable Power(int number, int exponent)
+        {
+            int valueCounter = 0;
+            int currentResult = 1;
+            while (valueCounter++ < exponent)
+            {
+                currentResult = currentResult * number;
+                yield return currentResult;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PowerList demoObj = new PowerList();
+
+            // Display powers of 2 up to the exponent 10
+            foreach (var value in demoObj.Power(2, 10))
+            {
+                Console.Write("{0} ", value);
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
+
